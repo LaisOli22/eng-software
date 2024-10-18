acccb9e899689a7eda25393fbf541e12ba475596
perishow <plm@poli.br>
2024-10-17 22:36:10 -0300

    primeira implementação da tela principal

--- LearnBotFront/src/pages/TopicSelection.jsx
+++ LearnBotFront/src/pages/TopicSelection.jsx
@@ -3,15 +3,27 @@ import ConceptExplainButton from '../components/ConceptExplainButton';
 import CodeCorrectionButton from '../components/CodeCorrectionButton';
 import CreateActivityButton from '../components/CreateActivityButton';
 import ButtonTemplate from '../components/ButtonTemplate';
+import Header from '../components/Header';
 
 function TopicSelection() {
   return (
     <NextUIProvider>
         <div className="bg-black h-screen bg-purple bg-cover bg-center">
-          <ConceptExplainButton/>
-          <CodeCorrectionButton/>
-          <CreateActivityButton/>
-          <ButtonTemplate/>
+          <Header></Header>
+          <div className='flex flex-col justify-center items-center'>
+            <p className='text-center text-3xl font-semibold'>Olá, eu sou o Edu.<br/>Como posso te ajudar?</p>
+            <img className='size-96' src='src/assets/IMG_5683.png'></img>
+          </div>
+          <div>
+            <div className='flex justify-center p-8 space-x-6'>
+                <ConceptExplainButton/>
+                <CodeCorrectionButton/>
+            </div>
+            <div className='flex justify-center space-x-6'>
+                <CreateActivityButton/>
+                <ButtonTemplate/>
+            </div>
+          </div>
         </div>
     </NextUIProvider>
   )
