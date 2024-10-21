e93ad47a3483b4be7eaaed7bc86b818c4145b9cb
LaisOli22 <laislsolalis@gmail.com>
2024-10-21 10:19:42 -0300

    Agrupei o Crud

--- Crud/routes/questionRoutes.js
+++ Crud/routes/questionRoutes.js
@@ -1,49 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Question = require('../models/question');
+const Question = require("../models/question");
 
 //Criar uma nova pergunta e resposta
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { question, answer } = req.body;
 
     const newQuestion = new Question({
       question,
-      answer
+      answer,
     });
 
     const savedQuestion = await newQuestion.save();
     res.json(savedQuestion);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao criar pergunta' });
+    res.status(500).json({ error: "Erro ao criar pergunta" });
   }
 });
 
 //Listar todas as perguntas
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const questions = await Question.find();
     res.json(questions);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao listar perguntas' });
+    res.status(500).json({ error: "Erro ao listar perguntas" });
   }
 });
 
 //Obter uma pergunta por ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const question = await Question.findById(req.params.id);
     if (!question) {
-      return res.status(404).json({ error: 'Pergunta não encontrada' });
+      return res.status(404).json({ error: "Pergunta não encontrada" });
     }
     res.json(question);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao obter a pergunta' });
+    res.status(500).json({ error: "Erro ao obter a pergunta" });
   }
 });
 
 //Atualizar uma pergunta e resposta
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const { question, answer } = req.body;
     const updatedQuestion = await Question.findByIdAndUpdate(
@@ -53,20 +53,20 @@ router.put('/:id', async (req, res) => {
     );
     res.json(updatedQuestion);
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao atualizar a pergunta' });
+    res.status(500).json({ error: "Erro ao atualizar a pergunta" });
   }
 });
 
 //Deletar uma pergunta por ID
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const deletedQuestion = await Question.findByIdAndDelete(req.params.id);
     if (!deletedQuestion) {
-      return res.status(404).json({ error: 'Pergunta não encontrada' });
+      return res.status(404).json({ error: "Pergunta não encontrada" });
     }
-    res.json({ message: 'Pergunta removida' });
+    res.json({ message: "Pergunta removida" });
   } catch (err) {
-    res.status(500).json({ error: 'Erro ao deletar a pergunta' });
+    res.status(500).json({ error: "Erro ao deletar a pergunta" });
   }
 });
 
