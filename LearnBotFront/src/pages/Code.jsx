f7b55258dd65c993bfa6fc5c1fd3d82982afb66b
perishow <plm@poli.br>
2024-11-02 14:54:43 -0300

    correção da besteira q eu fiz :(

--- LearnBotFront/src/pages/Code.jsx
+++ LearnBotFront/src/pages/Code.jsx
@@ -1,6 +1,5 @@
 import { NextUIProvider } from '@nextui-org/react';
 import Header from '../components/Header';
-import InputTab from '../components/InputTabCode';
 import CodeButtonContainer from '../components/CodeButtonsContainer';
 
 function Code() {
@@ -8,11 +7,6 @@ function Code() {
     <NextUIProvider>
         <div className="h-screen bg-green bg-cover bg-center">
         <Header/>
-<<<<<<< HEAD
-        <CodeInfo/>
-=======
-        <InputTab/>
->>>>>>> da6171a35ed559cba282a0f74a9eb7ba844550e9
         <CodeButtonContainer/>
       </div>
     </NextUIProvider>
--- LearnBotFront/src/pages/Explain.jsx
+++ LearnBotFront/src/pages/Explain.jsx
@@ -1,23 +1,13 @@
 import { NextUIProvider } from "@nextui-org/react";
 import Header from "../components/Header";
-import InputTab from "../components/InputTabExplain";
-<<<<<<< HEAD
-import ExplainInfo from "../components/ExplainInfo";
 import ExplainButtonContainer from "../components/ExplainButtonsContainer";
-=======
->>>>>>> da6171a35ed559cba282a0f74a9eb7ba844550e9
 
 function Explain() {
   return (
     <NextUIProvider>
       <div className="h-screen bg-purple bg-cover bg-center">
         <Header />
-<<<<<<< HEAD
-        <ExplainInfo/>
         <ExplainButtonContainer/>
-=======
-        <InputTab />
->>>>>>> da6171a35ed559cba282a0f74a9eb7ba844550e9
       </div>
     </NextUIProvider>
   );
